60e36d431eeda0e842113f1375b3dbe8a691623d
thomas lawrence <thomaslawrence@thomass-mbp.eduroam.student>
2023-10-17 12:52:35 +0100

    Add all buttons to the code where button switches them all on

--- RythmButton.ino
+++ RythmButton.ino
@@ -24,14 +24,23 @@
 
 // constants won't change. They're used here to set pin numbers:
 const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+const int blueRythmLedPin =  10;      // the number of the blue LED Pin which pulses at a set rythmn
+const int greenLedPin =  11;      // the number of the green LED Pin which outputs a success
+const int yellowLedPin =  12;      // the number of the green LED Pin which outputs a slight miss
+const int redLedPin =  13;      // the number of the green LED Pin which outputs a miss
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup() {
   // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
+  pinMode(blueRythmLedPin, OUTPUT);
+  // initialize the LED pin as an output:
+  pinMode(greenLedPin, OUTPUT);
+  // initialize the LED pin as an output:
+  pinMode(yellowLedPin, OUTPUT);
+  // initialize the LED pin as an output:
+  pinMode(redLedPin, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
 }
@@ -42,10 +51,16 @@ void loop() {
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
+    // turn all LED on:
+    digitalWrite(blueRythmLedPin, HIGH);
+    digitalWrite(greenLedPin, HIGH);
+    digitalWrite(yellowLedPin, HIGH);
+    digitalWrite(redLedPin, HIGH);
   } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
+    // turn all LED off:
+    digitalWrite(blueRythmLedPin, LOW);
+    digitalWrite(greenLedPin, LOW);
+    digitalWrite(yellowLedPin, LOW);
+    digitalWrite(redLedPin, LOW);
   }
 }
